6b20339b2cefb39713e58c806e5c42c41cf50cd9
GandyA23 <avila.gandy@gmail.com>
2021-03-02 08:55:25 -0600

    BootstrapVue 4 añadido al proyecto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue'
 
 Vue.config.productionTip = false
+// Importar las hojas de estilo de bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Vue debe de usar Bootstrap 
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
